228a29984b5570e41dc245d71b756f315f4a1cb9
LeroyR79 <leroy.robinson@education.nsw.gov.au>
2026-03-16 11:09:32 +1100

    Condensed code
    - Condense all of the "get__" functions into a singular function that gets all of the needed information on its own
    - Re-added the main_menu function to condense the code in main and make it look smoother

--- Functions.py
+++ Functions.py
@@ -6,15 +6,15 @@ if api_key == '':
     print("---------------NO API KEY--------------")
 
 
-def get_weather(city_name): #Function of getting the weather of the city
-    complete_url = f'{api_url}/current.json?key={api_key}&q={city_name}'
+def gatherAPI_Information(endpoint, city_name): #Function of getting the information of the city
+    complete_url = f'{api_url}/{endpoint}.json?key={api_key}&q={city_name}'
     response = requests.get(complete_url)
     if response.status_code == 200:
         return response.json()
     else:
         return None
 
-
+    
 def weather_info(weather_data): #Function of displaying the data
     if weather_data:
         location = weather_data["location"]["name"]
@@ -29,13 +29,7 @@ def weather_info(weather_data): #Function of displaying the data
     else:
         print("We encountered an error when accessing your weather. Your city may not exist or there's been a bug with the code, try again.")
 
-def get_timezone(city_name): #Function of getting a certain places timezone
-    timezoneurl = f'{api_url}/timezone.json?key={api_key}&q={city_name}'
-    response = requests.get(timezoneurl)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
+
 
 def Timezone_info(Timezone_data):     #Uses the information gathered in 'get_timezone' and displays it to the user
     if Timezone_data:
@@ -45,7 +39,38 @@ def Timezone_info(Timezone_data):     #Uses the information gathered in 'get_tim
     else: 
         print("We encountered an error when accessing your location's information")
 
-    
+def Forecast_info(Forecast_data):     #Uses the information gathered in 'get_forecast' and displays it to the user
+    if Forecast_data:
+        location = Forecast_data["location"]["name"]
+        region = Forecast_data["location"]["region"]
+        forecast = Forecast_data["forecast"]["forecastday"][0]["day"]
+        chanceofrain = forecast["daily_chance_of_rain"]
+        max_temp = forecast["maxtemp_c"]
+        min_temp = forecast["mintemp_c"]
+        print(f'The forecast for Today in {location}, {region}, has a max temperature of {max_temp}°C and a minimum of {min_temp}°C and a {chanceofrain}% chance of rain.')
+    else: 
+        print("We encountered an error when accessing your location's information")
+
+def main_menu():
+    print('Which would you like')
+    Choice = int(input("(1) View Current weather in city \n(2) View current time in a city \n(3) View daily forecast of a city \n(4) Quit \n"))
+    if Choice == 1:
+        city_name = input("Which city's weather would you like? ")
+        weather_data = gatherAPI_Information('current', city_name)
+        weather_info(weather_data)
+    elif Choice == 2:
+        city_name = input("Which city's time would you like? ")
+        Timezone_data = gatherAPI_Information('timezone', city_name)
+        Timezone_info(Timezone_data)
+    elif Choice == 3:
+        city_name = input("Which city's forecast would you like? ")
+        Forecast_data = gatherAPI_Information('forecast', city_name)
+        Forecast_info(Forecast_data)
+    elif Choice == 4:
+        print("Exiting... \n")
+        quit()
+    else:
+        print('Invalid choice, try again.')
 
 
     
--- main.py
+++ main.py
@@ -4,35 +4,11 @@ from Functions import *
 import time
 import tkinter as tk
 
-root = tk.Tk()
-root.title("Weather and time application")
-root.geometry("500x500")
-
-tk.Button(root, text="View Current weather in city", command=lambda: get_weather(input("Which city's weather would you like? "))).pack()
-tk.Button(root, text="View current time in a city", command=lambda: get_timezone(input("Which city's time would you like? "))).pack()
-tk.Button(root, text="Quit", command=root.quit).pack()
-
-root.mainloop()
+print("Welcome to the Weather application.")
 
 while True:
-    print("Welcome to the Data Gatherer")
-    print('Which would you like')
-    Choice = input("(1) View Current weather in city \n(2) View current time in a city \n(3) Quit \n")
-    if Choice == 3:
-        print("Exiting... \n")
-        quit()
-    elif Choice == 1:
-        city_name = input("Which city's weather would you like? ")
-        weather_data = get_weather(city_name)
-        weather_info(weather_data)
-    elif Choice == 2:
-        city_name = input("Which city's time would you like? ")
-        Timezone_data = get_timezone(city_name)
-        Timezone_info(Timezone_data)
-    else:
-        print('Invalid choice, try again.')
-    time.sleep(1)
-
+    
+    main_menu()
 
 
 
